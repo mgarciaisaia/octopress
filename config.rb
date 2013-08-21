1fd786d64e0246ef03c867847cbd03991a4b2696
Matias Garcia Isaia <mgarciaisaia@gmail.com>
2013-08-21 11:24:29 -0300

    Footnotes y autores

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/tutorial-c/"
+http_images_path = "/tutorial-c/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/tutorial-c/fonts"
+css_dir = "public/tutorial-c/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
